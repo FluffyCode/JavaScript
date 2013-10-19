3851166f0bffc6174c402c00707626d7ab8cebe6
Anthony Severini <FluffyCode@gmail.com>
2013-10-18 21:40:41 -0700

    Initial commit; play around with various data types

---
+++ basics/literals.js
@@ -0,0 +1,20 @@
+// Number
+var somenumber = 7;
+var somenumber = 12;
+
+// String
+var somestring = "I am a string";
+var somestring = 'I am a string, too';
+
+// Boolean
+var aboolean = true;
+var anotherboolean = false;
+
+// Array
+var somearray = ["fluffy bunnies", "waffles", 19];
+
+// Object
+var waffle={
+  crispyness : "Crispy, golden-brown goodness on the outside, melt-in-your-mouth soft on the inside",
+  size : "8-inch diameter"
+};
