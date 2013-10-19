b250bd2f143a846be0a94c1edcee715057e2e20b
Anthony Severini <FluffyCode@gmail.com>
2013-10-18 20:02:10 -0700

    Add a few more examples of writing to the html document

--- basics/docwrite.js
+++ basics/docwrite.js
@@ -3,3 +3,11 @@
 
 document.write("<h1>This is a heading</h1>")
 document.write("<p>This is a paragraph</p>")
+
+document.write("<br>") // spacing
+
+document.write("<ul>")
+document.write("<li>Waffles</li>")
+document.write("<li>Tacos</li>")
+document.write("<li>Fluffy Kittens</li>")
+document.write("</ul>")
