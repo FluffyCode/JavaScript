c5722e9346ce8711ec7cbe355ae674c98c6549c8
Anthony Severini <FluffyCode@gmail.com>
2013-10-18 19:58:25 -0700

    Add example of writing tags, information to html via a sourced script

---
+++ basics/docwrite.js
@@ -0,0 +1,5 @@
+// From JavaScript - The Definitive Guide, 6th Edition
+// "A literal is a data value that appears directly in a program."
+
+document.write("<h1>This is a heading</h1>")
+document.write("<p>This is a paragraph</p>")
