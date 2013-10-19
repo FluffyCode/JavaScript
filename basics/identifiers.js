ea33b078001d98c6190c3681ff91e7d44e247a79
Anthony Severini <FluffyCode@gmail.com>
2013-10-19 14:24:19 -0700

    Explore JS identifiers

---
+++ basics/identifiers.js
@@ -0,0 +1,11 @@
+// Identifiers are JavaScript's way of naming variables.
+
+// Identifiers in JavaScript must begin with a letter,
+// an underscore or a dollar sign. The following are all
+// legal identifiers:
+
+x
+some_variable
+_fluffy_bunnies
+v8juice
+$waffles
