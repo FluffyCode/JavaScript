34383c19f15303af52c5d40cf7580905e3e142c4
Anthony Severini <FluffyCode@gmail.com>
2013-10-19 17:25:55 -0700

    Change number-variable names; change value of one of them; add notes; touchup spacing

--- basics/literals.js
+++ basics/literals.js
@@ -1,17 +1,34 @@
 // Number
-var somenumber = 7;
-var anothernumber = 12;
+var num1 = 7;
+var num2 = 4.7;
+
+// Unlike other languages (such as Ruby), JavaScript does not differentiate between
+// integers and floating-point numbers. JavaScript treats all numbers as floating-point.
+
+	// typeof num1
+		// => number
+
+	// typeof num2
+		// => number
+
+
 
 // String
 var somestring = "I am a string";
 var anotherstring = 'I am a string, too';
 
+
+
 // Boolean
 var aboolean = true;
 var anotherboolean = false;
 
+
+
 // Array
 var somearray = ["fluffy bunnies", "waffles", 19];
+
+
 
 // Object
 var waffle={
