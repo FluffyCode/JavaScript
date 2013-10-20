009e9475e6a71e1fde2e03e412c571c5b74c4b8b
Anthony Severini <FluffyCode@gmail.com>
2013-10-20 09:20:19 -0700

    Replace 'header' <p> tags with <h1> tags

--- basics/arithmetic/test_arithmetic.js
+++ basics/arithmetic/test_arithmetic.js
@@ -2,7 +2,7 @@
 
 // Exponents
 // The first argument is the base number; the second is the exponent
-  document.write("<p class='h'>Exponents</p>");
+  document.write("<h1>Exponents</h1>");
   document.write("<p class='i'>Math.pow(3,2) = " + Math.pow(3,2) + "</p>");
     // => 9
   document.write("<p class='i'>Math.pow(4,3) = " + Math.pow(4,3) + "</p>");
@@ -11,7 +11,7 @@
 document.write("<br>");
 
 // Rounding, rounding up and rounding down
-  document.write("<p class='h'>Rounding</p>");
+  document.write("<h1>Rounding</h1>");
   document.write("<p class='i'>Math.round(.4) = " + Math.round(.4) + "</p>")
     // => 0
   document.write("<p class='i'>Math.round(.5) = " + Math.round(.5) + "</p>")
