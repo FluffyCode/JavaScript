2f14637d28dfa7ed073e9090d256facb826d71e8
Anthony Severini <FluffyCode@gmail.com>
2013-10-20 09:21:26 -0700

    Remove class from <p> tags

--- basics/arithmetic/test_arithmetic.js
+++ basics/arithmetic/test_arithmetic.js
@@ -3,18 +3,18 @@
 // Exponents
 // The first argument is the base number; the second is the exponent
   document.write("<h1>Exponents</h1>");
-  document.write("<p class='i'>Math.pow(3,2) = " + Math.pow(3,2) + "</p>");
+  document.write("<p>Math.pow(3,2) = " + Math.pow(3,2) + "</p>");
     // => 9
-  document.write("<p class='i'>Math.pow(4,3) = " + Math.pow(4,3) + "</p>");
+  document.write("<p>Math.pow(4,3) = " + Math.pow(4,3) + "</p>");
     // => 64
 
 document.write("<br>");
 
 // Rounding, rounding up and rounding down
   document.write("<h1>Rounding</h1>");
-  document.write("<p class='i'>Math.round(.4) = " + Math.round(.4) + "</p>")
+  document.write("<p>Math.round(.4) = " + Math.round(.4) + "</p>")
     // => 0
-  document.write("<p class='i'>Math.round(.5) = " + Math.round(.5) + "</p>")
+  document.write("<p>Math.round(.5) = " + Math.round(.5) + "</p>")
     // => 1
-  document.write("<p class='i'>Math.round(.6) = " + Math.round(.6) + "</p>")
+  document.write("<p>Math.round(.6) = " + Math.round(.6) + "</p>")
     // => 1
