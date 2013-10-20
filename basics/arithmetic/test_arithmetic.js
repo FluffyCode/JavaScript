8f1806139ad216ff73d3bcdb8edd22a860bd8d04
Anthony Severini <FluffyCode@gmail.com>
2013-10-20 09:11:56 -0700

    Add classes to <p> tags

--- basics/arithmetic/test_arithmetic.js
+++ basics/arithmetic/test_arithmetic.js
@@ -2,8 +2,10 @@
 
 // Exponents
 // The first argument is the base number; the second is the exponent
-document.write("<p>Exponents</p>");
-document.write("<p>Math.pow(3,2) = " + Math.pow(3,2) + "</p>");
+document.write("<p class='h'>Exponents</p>");
+document.write("<p class='i'>Math.pow(3,2) = " + Math.pow(3,2) + "</p>");
   // => 9
-document.write("<p>Math.pow(4,3) = " + Math.pow(4,3) + "</p>");
+document.write("<p class='i'>Math.pow(4,3) = " + Math.pow(4,3) + "</p>");
   // => 64
+
+document.write("<br>")
