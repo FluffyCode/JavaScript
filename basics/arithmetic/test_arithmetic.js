37ab7b0a42739a1e878c44d08424e781dbaa4ea0
Anthony Severini <FluffyCode@gmail.com>
2013-10-20 09:17:46 -0700

    Touchup indentation; add section for general rounding

--- basics/arithmetic/test_arithmetic.js
+++ basics/arithmetic/test_arithmetic.js
@@ -2,10 +2,19 @@
 
 // Exponents
 // The first argument is the base number; the second is the exponent
-document.write("<p class='h'>Exponents</p>");
-document.write("<p class='i'>Math.pow(3,2) = " + Math.pow(3,2) + "</p>");
-  // => 9
-document.write("<p class='i'>Math.pow(4,3) = " + Math.pow(4,3) + "</p>");
-  // => 64
+  document.write("<p class='h'>Exponents</p>");
+  document.write("<p class='i'>Math.pow(3,2) = " + Math.pow(3,2) + "</p>");
+    // => 9
+  document.write("<p class='i'>Math.pow(4,3) = " + Math.pow(4,3) + "</p>");
+    // => 64
 
-document.write("<br>")
+document.write("<br>");
+
+// Rounding, rounding up and rounding down
+  document.write("<p class='h'>Rounding</p>");
+  document.write("<p class='i'>Math.round(.4) = " + Math.round(.4) + "</p>")
+    // => 0
+  document.write("<p class='i'>Math.round(.5) = " + Math.round(.5) + "</p>")
+    // => 1
+  document.write("<p class='i'>Math.round(.6) = " + Math.round(.6) + "</p>")
+    // => 1
