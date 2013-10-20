5c8b42e7cc7711f6c612ab1565bce552a87e771e
Anthony Severini <FluffyCode@gmail.com>
2013-10-19 17:49:20 -0700

    Add examples for exponential math

--- basics/test_arithmetic.js
+++ basics/test_arithmetic.js
@@ -1,1 +1,9 @@
 // Playing around with maths in JavaScript
+
+// Exponents
+// The first argument is the base number; the second is the exponent
+document.write("<p>Math.pow(3,2) = " + Math.pow(3,2) + "</p>");
+  // => 9
+
+document.write("<p>Math.pow(4,3) = " + Math.pow(4,3) + "</p>");
+  // => 64
