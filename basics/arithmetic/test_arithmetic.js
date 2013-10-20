24ead86f2bc59fc860e323752741999c045865ea
Anthony Severini <FluffyCode@gmail.com>
2013-10-19 18:36:24 -0700

    Add 'header' for Exponents section of math

--- basics/arithmetic/test_arithmetic.js
+++ basics/arithmetic/test_arithmetic.js
@@ -2,8 +2,8 @@
 
 // Exponents
 // The first argument is the base number; the second is the exponent
+document.write("<p>Exponents</p>");
 document.write("<p>Math.pow(3,2) = " + Math.pow(3,2) + "</p>");
   // => 9
-
 document.write("<p>Math.pow(4,3) = " + Math.pow(4,3) + "</p>");
   // => 64
