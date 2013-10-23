60654494bc610be1f79adfa8061fea79965f5e1c
Anthony Severini <FluffyCode@gmail.com>
2013-10-23 10:57:17 -0700

    Add examples of forcing a Date object's day forward by advancing the hours beyond 24

--- basics/date_time/test_datetime.js
+++ basics/date_time/test_datetime.js
@@ -11,8 +11,23 @@ document.write("<p>" + some_date + "</p>")
 document.write("<br>")
 
 // Setting time with the date
-some_date = new Date(2011, 2, 16, 12, 30, 00)
+some_date = new Date(2011, 2, 16, 12, 30, 0)
 
 document.write("<h1>Creating a Date object with a set time</h1>")
 document.write("<p>" + some_date + "</p>")
   // => Wed Mar 16 2011 12:30:00 GMT-0700 (PDT)
+
+document.write("<br>")
+
+// Note that when creating a Date object with a set time, the fourth argument,
+// hours, works in 24-hour time. Setting the hour-time in excess of 24-hour
+// chunks can even force the Date object to advance days:
+some_date = new Date(2013, 9, 23, 0, 0, 0)
+document.write("<p>" + some_date + "</p>")
+  // => Wed Oct 23 2013 00:00:00 GMT-0700 (PDT)
+some_date = new Date(2013, 9, 23, 24, 0, 0)
+document.write("<p>" + some_date + "</p>")
+  // => Thu Oct 24 2013 00:00:00 GMT-0700 (PDT)
+some_date = new Date(2013, 9, 23, 48, 0, 0)
+document.write("<p>" + some_date + "</p>")
+  // => Fri Oct 25 2013 00:00:00 GMT-0700 (PDT)
