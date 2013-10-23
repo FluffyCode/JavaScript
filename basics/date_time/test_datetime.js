3b08226fccff7e0678ac39bd9c03bd3daa7551dc
Anthony Severini <FluffyCode@gmail.com>
2013-10-23 09:41:44 -0700

    Add basic example of Date

---
+++ basics/date_time/test_datetime.js
@@ -0,0 +1,8 @@
+// Basic date (year, month, day)
+var some_date = new Date(2011, 2, 16)
+// Note that the month is indexed:
+// Jan = 0, Feb = 1, Mar = 2, Apr = 3, May = 4, Jun = 5
+// Jul = 6, Aug = 7, Sep = 8, Oct = 9, Nov = 10, Dec = 11
+
+document.write("<p>" + some_date + "</p>")
+  // => Wed Mar 16 2011 00:00:00 GMT-0700 (PDT)
