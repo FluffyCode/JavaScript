c37becdcf2a3686bcff15d3e341078da46a34e8b
Anthony Severini <FluffyCode@gmail.com>
2013-10-26 18:27:11 -0700

    Add examples for strings in single- and double-quotes

--- basics/text/test_text.js
+++ basics/text/test_text.js
@@ -3,6 +3,12 @@ document.write("<h1>String literals</h1>")
 var some_string_literal = ""
 document.write("<p>An empty string: " + some_string_literal + "</p>")
 
+var some_string_literal = "chicken and waffles"
+document.write("<p>A string in double quotes: " + some_string_literal + "</p>")
+
+var some_string_literal = 'waffles and chicken'
+document.write("<p>A string in single quotes: " + some_string_literal + "</p>")
+
 
 
 document.write("<p></p>")
