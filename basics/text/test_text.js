0874e668976493e27274d8741f5e121f4824edb9
Anthony Severini <FluffyCode@gmail.com>
2013-10-27 09:46:23 -0700

    Add section headers; touchup spacing, indentation

--- basics/text/test_text.js
+++ basics/text/test_text.js
@@ -1,41 +1,43 @@
 document.write("<h1>String literals</h1>")
+  var some_string_literal = ""
+  document.write("<p>An empty string: " + some_string_literal + "</p>")
 
-var some_string_literal = ""
-document.write("<p>An empty string: " + some_string_literal + "</p>")
+  var some_string_literal = "chicken and waffles"
+  document.write("<p>A string in double quotes: " + some_string_literal + "</p>")
 
-var some_string_literal = "chicken and waffles"
-document.write("<p>A string in double quotes: " + some_string_literal + "</p>")
+  var some_string_literal = 'waffles and chicken'
+  document.write("<p>A string in single quotes: " + some_string_literal + "</p>")
 
-var some_string_literal = 'waffles and chicken'
-document.write("<p>A string in single quotes: " + some_string_literal + "</p>")
+  var some_string_literal = "7.75"
+  document.write("<p>A number between quotes: " + some_string_literal + "</p>")
 
-var some_string_literal = "7.75"
-document.write("<p>A number between quotes: " + some_string_literal + "</p>")
+  var some_string_literal = "Singin' in the rain"
+  document.write("<p>Single quotes within a double-quote string: " + some_string_literal + "</p>")
 
-var some_string_literal = "Singin' in the rain"
-document.write("<p>Single quotes within a double-quote string: " + some_string_literal + "</p>")
+  var some_string_literal = 'A "glorious feeling," say reviews'
+  document.write("<p>Double quotes within a single-quote string: " + some_string_literal + "</p>")
 
-var some_string_literal = 'A "glorious feeling," say reviews'
-document.write("<p>Double quotes within a single-quote string: " + some_string_literal + "</p>")
+  var some_string_literal = 'I\'m singin\' in the rain'
+  document.write("<p>Escaping non-delimited single-quotes within a single-quote-delimited string: " + some_string_literal + "</p>")
 
-var some_string_literal = 'I\'m singin\' in the rain'
-document.write("<p>Escaping non-delimited single-quotes within a single-quote-delimited string: " + some_string_literal + "</p>")
-
-var some_string_literal = "He said, \"I get it, you're singing in the rain.\""
-document.write("<p>Escaping non-delimited double-quotes within a double-quote-delimited string: " + some_string_literal + "</p>")
-
-document.write("<br>")
-
-var string1 = "Hello"
-var string2 = "world"
-
-document.write("<p>Concatenating strings: " + string1 + ", " + string2 + "</p>")
+  var some_string_literal = "He said, \"I get it, you're singing in the rain.\""
+  document.write("<p>Escaping non-delimited double-quotes within a double-quote-delimited string: " + some_string_literal + "</p>")
 
 document.write("<br>")
 
-var really_long_string = "supercalafragalisticexpialadoshus"
-document.write("<p>" + really_long_string + "</p>")
-document.write("<p>Getting the length of a string: " + really_long_string.length + "</p>")
+document.write("<h1>String concatenation</h1>")
+  var string1 = "Hello"
+  var string2 = "world"
+
+  document.write("<p>Concatenating strings: " + string1 + ", " + string2 + "</p>")
+
+document.write("<br>")
+
+document.write("<h1>String length</h1>")
+  var really_long_string = "supercalafragalisticexpialadoshus"
+
+  document.write("<p>" + really_long_string + "</p>")
+  document.write("<p>Getting the length of a string: " + really_long_string.length + "</p>")
 
 
 
