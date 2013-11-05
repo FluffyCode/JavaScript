e7a1b0d136f3da09c0f5ef0d1d69ca1556cce782
Anthony Severini <FluffyCode@gmail.com>
2013-11-05 10:20:04 -0800

    Remove unnecessary repeated use of 'var'

--- basics/text/test_text.js
+++ basics/text/test_text.js
@@ -2,25 +2,25 @@ document.write("<h1>String literals</h1>")
   var some_string_literal = ""
   document.write("<p>An empty string: " + some_string_literal + "</p>")
 
-  var some_string_literal = "chicken and waffles"
+  some_string_literal = "chicken and waffles"
   document.write("<p>A string in double quotes: " + some_string_literal + "</p>")
 
-  var some_string_literal = 'waffles and chicken'
+  some_string_literal = 'waffles and chicken'
   document.write("<p>A string in single quotes: " + some_string_literal + "</p>")
 
-  var some_string_literal = "7.75"
+  some_string_literal = "7.75"
   document.write("<p>A number between quotes: " + some_string_literal + "</p>")
 
-  var some_string_literal = "Singin' in the rain"
+  some_string_literal = "Singin' in the rain"
   document.write("<p>Single quotes within a double-quote string: " + some_string_literal + "</p>")
 
-  var some_string_literal = 'A "glorious feeling," say reviews'
+  some_string_literal = 'A "glorious feeling," say reviews'
   document.write("<p>Double quotes within a single-quote string: " + some_string_literal + "</p>")
 
-  var some_string_literal = 'I\'m singin\' in the rain'
+  some_string_literal = 'I\'m singin\' in the rain'
   document.write("<p>Escaping non-delimited single-quotes within a single-quote-delimited string: " + some_string_literal + "</p>")
 
-  var some_string_literal = "He said, \"I get it, you're singing in the rain.\""
+  some_string_literal = "He said, \"I get it, you're singing in the rain.\""
   document.write("<p>Escaping non-delimited double-quotes within a double-quote-delimited string: " + some_string_literal + "</p>")
 
 document.write("<br>")
